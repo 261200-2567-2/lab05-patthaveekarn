1108fdc857ab0c9cd4dc984d255eafd21342b020
patthaveekarn <patthaveekarn2544@gmail.com>
2024-12-02 23:54:00 +0700

    change main,accessary,Character

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,13 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-
     public static void main(String[] args) {
-        // Create characters
         RPGCharacter warrior = new paladin("Conan", 100, 50);
         RPGCharacter mage = new wizard("Merlin", 80, 100);
 
-        // Create accessories
         accessory necklace = new necklace();
         accessory ring = new ring();
 
-        // Simulate interactions
         warrior.attack(mage);
         mage.attack(warrior);
 
@@ -19,9 +15,9 @@ public class Main {
         mage.useMana(20);
 
         necklace.applyEffect(warrior);
-        ring.applyEffect(warrior);
+        ring.useSkill(warrior);
 
-        System.out.println("\nStatus:");
+        System.out.println("\nFinal Status:");
         warrior.showStatus();
         mage.showStatus();
     }
